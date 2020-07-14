55a32566dffe5dd47909278e93838825375ceafd
MatiasWettstein <matiasjwettstein@gmail.com>
2020-07-14 14:47:23 -0300

    creado el DBManager y la conexion a la base de datos, en el main hay un metodo para ver si funciona

--- TPIntegradorDIED/src/main/java/imp/domain/App.java
+++ TPIntegradorDIED/src/main/java/imp/domain/App.java
@@ -1,5 +1,12 @@
 package imp.domain;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.swing.JOptionPane;
+
+import imp.managers.DBManager;
+
 /**
  * Hello world!
  *
@@ -8,6 +15,20 @@ public class App
 {
     public static void main( String[] args )
     {
+    	try {
+
+			DBManager dbm = DBManager.getInstance();
+			Connection con = dbm.getConn();
+			JOptionPane.showMessageDialog(null, "Base de datos conectada.", "Conexión establecida", JOptionPane.INFORMATION_MESSAGE);
+			con.close();
+		} catch (SQLException e) {
+			JOptionPane.showMessageDialog(null, "No Hay Conexión con la Base de Datos.", "Error", JOptionPane.WARNING_MESSAGE);
+			//e.printStackTrace();
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, "No Hay Conexión con la Base de Datos.", "Error", JOptionPane.WARNING_MESSAGE);
+			//e.printStackTrace();
+		}
+
         imprimeMensaje("Hello World!");
     }
     
