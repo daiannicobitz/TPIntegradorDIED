0d29aa04162834d83e59edde3b85eda2b2e96518
claraMazzi <mazzimclara@gmail.com>
2020-07-17 17:28:19 -0300

    crear paquetes de gestores e interfaces

---
+++ TPIntegradorDIED/src/main/java/imp/interfaces/MenuPrincipal.java
@@ -0,0 +1,16 @@
+package imp.interfaces;
+
+import javax.swing.JFrame;
+
+public class MenuPrincipal extends JFrame {
+
+	public static void start () {
+		MenuPrincipal menu = new MenuPrincipal();
+	}
+	
+	
+	
+	
+	
+	
+}
