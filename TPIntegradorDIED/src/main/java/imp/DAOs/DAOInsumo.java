f6bc19e032fd9f277b95605086a46e7f0cd6045e
daiannicobitz <daianbitz98@gmail.com>
2020-07-18 14:22:12 -0300

    sda

--- TPIntegradorDIED/src/main/java/imp/DAOs/DAOInsumo.java
+++ TPIntegradorDIED/src/main/java/imp/DAOs/DAOInsumo.java
@@ -6,11 +6,15 @@ import java.sql.Statement;
 
 import imp.managers.DBManager;
 import imp.primaryClasses.Insumo;
+import imp.primaryClasses.InsumoGeneral;
+import imp.primaryClasses.InsumoLiquido;
 
 public class DAOInsumo {
 	
 	public static void EliminarInsumo(Insumo insumo) {
 
+//		daian
+		
 		DBManager dbm = DBManager.getInstance();
 		Connection con = dbm.getConn();
 		
@@ -22,6 +26,37 @@ public class DAOInsumo {
 			
 			st.close();
 			con.close();
+		} catch (SQLException e) {
+			e.getMessage();
+		}
+		
+	}
+	
+	public static void AltaInsumo(Insumo insumo) {
+		
+//		daian
+		
+		DBManager dbm = DBManager.getInstance();
+		Connection con = dbm.getConn();
+		String consulta = null;
+		
+		try {
+			if(insumo instanceof InsumoGeneral) {
+				consulta = "insert into insumo(cantidad,costo_unitario,densidad,descripcion,peso,unidad_medida) "
+					+ "values ('"+insumo.getCantidad()+"',null,'"+insumo.getDescripcion()+"',"
+							+ "'"+((InsumoGeneral) insumo).getPeso()+"','"+insumo.getUnidadMedida().toString()+"')";
+				
+			} else {
+				consulta = "insert into insumo(cantidad,costo_unitario,densidad,descripcion,peso,unidad_medida) "
+						+ "values ('"+insumo.getCantidad()+"','"+((InsumoLiquido) insumo).getDensidad()+"','"+insumo.getDescripcion()+"',"
+								+ "null,'"+insumo.getUnidadMedida().toString()+"')";
+			}
+			Statement st = con.createStatement();
+			int nro = st.executeUpdate(consulta);
+			
+			st.close();
+			con.close();
+			
 		} catch (SQLException e) {
 			e.getMessage();
 		}
