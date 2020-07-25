6d5a8008bfb20e76605ff6e95e2bb4cc80d3310c
claraMazzi <mazzimclara@gmail.com>
2020-07-25 15:15:17 -0300

    Pantallas orden pedido e insumos terminadas

--- TPIntegradorDIED/src/main/java/imp/interfaces/DarAltaInsumo.java
+++ TPIntegradorDIED/src/main/java/imp/interfaces/DarAltaInsumo.java
@@ -33,18 +33,18 @@ public class DarAltaInsumo extends JPanel {
 		lbl_descripcion.setBounds(125, 60, 91, 14);
 		
 		JLabel lbl_unidadMedida = new JLabel("UNIDAD DE MEDIDA");
-		lbl_unidadMedida.setBounds(125, 123, 111, 14);
+		lbl_unidadMedida.setBounds(125, 168, 111, 14);
 		
 		JComboBox combo_medidas = new JComboBox();
-		combo_medidas.setBounds(265, 120, 160, 20);
+		combo_medidas.setBounds(265, 165, 160, 20);
 		combo_medidas.setModel(new DefaultComboBoxModel(UM.values()));
 		
 		
 		JLabel lbl_peso = new JLabel("PESO");
-		lbl_peso.setBounds(519, 208, 91, 14);
+		lbl_peso.setBounds(519, 214, 91, 14);
 		
 		JTextField txt_peso = new JTextField();
-		txt_peso.setBounds(657, 205, 168, 20);
+		txt_peso.setBounds(657, 211, 168, 20);
 		txt_peso.setColumns(10);
 		
 		JFormattedTextField ftxt_descripcion = new JFormattedTextField();
@@ -70,16 +70,16 @@ public class DarAltaInsumo extends JPanel {
 		
 		
 		JSpinner spinner_cantidad = new JSpinner();
-		spinner_cantidad.setBounds(265, 205, 160, 20);
+		spinner_cantidad.setBounds(265, 211, 160, 20);
 		
 		JLabel lbl_cantidad = new JLabel("CANTIDAD");
-		lbl_cantidad.setBounds(125, 208, 135, 14);
+		lbl_cantidad.setBounds(125, 214, 135, 14);
 		
 		JLabel lbl_costoUnitario = new JLabel("COSTO UNITARIO");
-		lbl_costoUnitario.setBounds(519, 123, 108, 14);
+		lbl_costoUnitario.setBounds(519, 168, 108, 14);
 		
 		JFormattedTextField ftxt_costoUnitario = new JFormattedTextField();
-		ftxt_costoUnitario.setBounds(657, 120, 168, 20);
+		ftxt_costoUnitario.setBounds(657, 165, 168, 20);
 		
 		ftxt_costoUnitario.addKeyListener(new KeyAdapter() {
 			
@@ -99,10 +99,10 @@ public class DarAltaInsumo extends JPanel {
 		});
 		
 		JLabel lbl_densidad = new JLabel("DENSIDAD");
-		lbl_densidad.setBounds(125, 289, 125, 14);
+		lbl_densidad.setBounds(125, 260, 125, 14);
 		
 		JFormattedTextField ftxt_densidad = new JFormattedTextField();
-		ftxt_densidad.setBounds(265, 286, 160, 20);
+		ftxt_densidad.setBounds(265, 257, 160, 20);
 		ftxt_densidad.addKeyListener(new KeyAdapter() {
 			
 			@Override
@@ -136,7 +136,7 @@ public class DarAltaInsumo extends JPanel {
 		add(txt_peso);
 		
 		JButton btn_aceptar = new JButton("ACEPTAR");
-		btn_aceptar.setBounds(529, 397, 98, 40);
+		btn_aceptar.setBounds(619, 368, 98, 40);
 		btn_aceptar.setFont(new Font("Montserrat", Font.ITALIC, 11));
 		btn_aceptar.setBorderPainted(false);
 		btn_aceptar.setFocusPainted(false);
@@ -146,7 +146,7 @@ public class DarAltaInsumo extends JPanel {
 		add(btn_aceptar);
 		
 		JButton btn_cancelar = new JButton("CANCELAR");
-		btn_cancelar.setBounds(637, 397, 98, 40);
+		btn_cancelar.setBounds(727, 368, 98, 40);
 		btn_cancelar.setFont(new Font("Montserrat", Font.ITALIC, 11));
 		btn_cancelar.setBorderPainted(false);
 		btn_cancelar.setFocusPainted(false);
@@ -154,6 +154,21 @@ public class DarAltaInsumo extends JPanel {
 		btn_cancelar.setForeground(new Color(0, 0, 0));
 		btn_cancelar.setBackground(new Color(80, 165, 94));
 		add(btn_cancelar);
+		
+		JLabel lbl_planta = new JLabel("PLANTA");
+		lbl_planta.setBounds(125, 122, 46, 14);
+		add(lbl_planta);
+		
+		JComboBox combo_planta = new JComboBox();
+		combo_planta.setBounds(265, 118, 160, 22);
+		add(combo_planta);
+		
+		JLabel lbl_puntoP = new JLabel("PUNTO PEDIDO");
+		lbl_puntoP.setBounds(519, 122, 91, 14);
+		add(lbl_puntoP);
+		
+		JSpinner spinner_puntoP = new JSpinner();
+		spinner_puntoP.setBounds(657, 119, 168, 20);
+		add(spinner_puntoP);
 	}
-
 }
